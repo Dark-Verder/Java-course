e9facdfb281788f7dd17b8fec036ba39b67853fb
Lada <vladislava.oblogina@gmail.com>
2026-03-02 09:30:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+public static void printThreeWords (){
+System.out.println("Orange");
+System.out.println("Banana");
+System.out.println("Apple");
+}
+public static void main(String[]args){
+printThreeWords();
+}
+}
